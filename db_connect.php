29636998a17d5ba999fb778ebceb2c5b6604e3e5
xXJsXx <miloice610@gmail.com>
2025-01-25 00:21:02 +0800

    update password

--- db_connect.php
+++ db_connect.php
@@ -1,5 +1,5 @@
 <?php
-$conn = new mysqli("localhost", "root", "", "tcc");
+$conn = new mysqli("localhost", "root", "TCCProject", "tcc");
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
